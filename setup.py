8dd14955b7b1229a40ea9511145f9054c2e5c78b
SoJS coder <77751154+sojs-coder@users.noreply.github.com>
2023-01-26 05:00:03 +0000

    small marckdown patch

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
+long_description = open("README.md").read()
 
 setup(
     name='SoAuth',
-    version='1.0.0',
+    version='1.0.2',
     description='Flask middleware for SoAuth',
     url='https://github.com/sojs-coder/SoAuth-Py',
     author='SoJS',
     author_email='sojs_coder@protonmail.com',
     license='ISC',
     packages=['soauth'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'requests',
         'flask'
